446dbe48b8c54a1e7ace9a5bdfd186f9e42dbffc
Andrea Terenziani <andrea.terenziani.at@gmail.com>
2019-11-22 15:39:25 +0100

    Misc fixes
    1) improved play dialog and exit dialog
    2) misc

--- src/GridContainer.gd
+++ src/GridContainer.gd
@@ -22,12 +22,20 @@ func _ready():
 		passedLevel = Settings.min_difficulty
 	tick_max = 1 - range_lerp(passedLevel, Settings.min_difficulty, Settings.max_difficulty, 0.5, 0.8)
 	table = Table.new(passedLevel)
-	table.connect("lines_cleared", self, "handle_lines_cleared")
-	table.connect("level_up", self, "handle_level_up")
+	table.connect("newTableAction", self, "handleTableAction")
 	redraw()
 
 func toggle_pause(p):
 	paused = p
+
+func handleTableAction(action) :
+	
+	emit_signal("newTableAction", action)
+	
+	handle_lines_cleared(action)
+	
+	if action == Table.TABLE_ACTION.LEVEL_UP :
+		handle_level_up()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -38,32 +46,25 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("hard_drop"):
 		table.hard_drop()
-		emit_signal("newTableAction", Table.TABLE_ACTION.HARD_DROP)
 		tick_timer = tick_max/2
 	if Input.is_action_just_pressed("soft_drop"):
 		table.tick()
-		emit_signal("newTableAction", Table.TABLE_ACTION.SOFT_DROP)
 		tick_timer = 0#tick_max/2
 		
 	if Input.is_action_just_pressed("hold"):
 		table.hold_tetromino()
-		emit_signal("newTableAction", Table.TABLE_ACTION.HOLD)
 		tick_timer = tick_max/4
 	if Input.is_action_just_pressed("spin_left"):
 		table.try_rotating_left()
-		emit_signal("newTableAction", Table.TABLE_ACTION.SPIN_L)
 		tick_timer = tick_max/4
 	if Input.is_action_just_pressed("spin_right"):
 		table.try_rotating_right()
-		emit_signal("newTableAction", Table.TABLE_ACTION.SPIN_R)
 		tick_timer = tick_max/4
 	if Input.is_action_just_pressed("move_left"):
 		table.try_moving_left()
-		emit_signal("newTableAction", Table.TABLE_ACTION.MOVE_L)
 		tick_timer = tick_max/4
 	if Input.is_action_just_pressed("move_right"):
 		table.try_moving_right()
-		emit_signal("newTableAction", Table.TABLE_ACTION.MOVE_R)
 		tick_timer = tick_max/4
 		
 	self.redraw()
@@ -89,27 +90,23 @@ func redraw():
 					texrect.texture = Tetromino.get_texture_from_value(value)
 			add_child(texrect)
 
-func handle_lines_cleared(amount):
-	var text = ["SINGLE!", "DOUBLE!", "TRIPLE!", "TETRIS!"][amount-1]
+func handle_lines_cleared(action):
+	var amount = 0
 	
-	var action = 0
+	match action :
+		Table.TABLE_ACTION.SINGLE_CLEAR : amount = 1
+		Table.TABLE_ACTION.DOUBLE_CLEAR : amount = 2
+		Table.TABLE_ACTION.TRIPLE_CLEAR : amount = 3
+		Table.TABLE_ACTION.TETRIS : amount = 4
 	
-	match amount :
-		1 : action = Table.TABLE_ACTION.SINGLE_CLEAR
-		2 : action = Table.TABLE_ACTION.DOUBLE_CLEAR
-		3 : action = Table.TABLE_ACTION.TRIPLE_CLEAR
-		4 : action = Table.TABLE_ACTION.TETRIS
-	
-	emit_signal("newTableAction", action)
-	emit_signal("newScore", table.total_lines_cleared)
-	
-	get_tree().root.add_child(Alert.show_alert(text))
+	if amount != 0 :
+		emit_signal("newTableAction", action)
+		emit_signal("newScore", table.total_lines_cleared)
+		
+		var text = ["SINGLE!", "DOUBLE!", "TRIPLE!", "TETRIS!"][amount-1]
+		get_tree().root.add_child(Alert.show_alert(text))
 
 
 func handle_level_up():
 	yield(get_tree().create_timer(1.4), "timeout")
-	
-	emit_signal("newTableAction", Table.TABLE_ACTION.LEVEL_UP)
-	emit_signal("newLevel", table.difficulty_level)
-	
 	get_tree().root.add_child(Alert.show_alert("Level up!"))
